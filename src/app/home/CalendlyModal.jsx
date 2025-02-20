59d27c6691de67559c8001f04fbcd62069cd5393
GhulamHaider599 <ghulam.haider@volticinc.dev>
2025-02-20 12:35:58 +0500

    SOme tweaks in calendly

--- src/app/home/CalendlyModal.jsx
+++ src/app/home/CalendlyModal.jsx
@@ -26,7 +26,7 @@ const CalendlyModal = ({ closeModal }) => {
             <style jsx>{`
         .popup-overlay {
           position: fixed;
-          top: 0;
+          top: 400;
           left: 0;
           right: 0;
           bottom: 0;
